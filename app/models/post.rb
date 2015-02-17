f0413d769070bf36f97d42f08b56d740fc48373b
Ahmed <ahmedfay@hotmail.com>
2015-02-16 20:47:00 -0500

    Scaffold new app with PG as db, using friendly_id and friendly title

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ActiveRecord::Base
+  default_scope -> { order('created_at DESC') }
+
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :history]
+
+  # valicdations
+  validates_presence_of :title, :slug, :intro, :content
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
+end
