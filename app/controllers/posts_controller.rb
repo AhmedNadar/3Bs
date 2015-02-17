e03402a8bab7fa46ee0ee285b2a1e67779eaf162
Ahmed <ahmedfay@hotmail.com>
2015-02-17 01:41:10 -0500

    Add simple Admin page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :set_post_redirect , only: [:show]
+
   http_basic_authenticate_with name: ENV["BLOG_USERNAME"],
                                password: ENV["BLOG_PASSWORD"],
                                except: [:show]
@@ -8,6 +9,11 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+    @posts = Post.all
+  end
+
+  # GET /posts for Admin
+  def admin
     @posts = Post.all
   end
 
